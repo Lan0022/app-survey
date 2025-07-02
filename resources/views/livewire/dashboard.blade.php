9ebef80df54fb86734f2cbe96832e077654dc873
Lan0022 <afdalart2@gmail.com>
2025-07-02 15:07:55 +0700

    Integrate NiceAdmin Dashboard panel

---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+            <h1>Dashboard</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    {{-- <li class="breadcrumb-item"><a href="index.html">Home</a></li> --}}
+                    <li class="breadcrumb-item active"><a wire:navigate href="#">Home</a></li>
+                </ol>
+            </nav>
+        </div><!-- End Page Title -->
+        {{-- Conten will be here --}}
+        <section class="section dashboard">
+
+        </section>
+
+    </main><!-- End #main -->
+    {{-- Success is as dangerous as failure. --}}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Auth\Login;
+use App\Livewire\Dashboard;
 
 Route::get('/', Login::class);
+Route::get('/dashboard', Dashboard::class)->name('dashboard');
